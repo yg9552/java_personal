bee8a9c72097b6e4f80ab1e3d20f1bbde6b9f4ac
yg9552 <yg4583@naver.com>
2022-06-30 17:26:00 +0900

    .

--- src/java_personal/tt/Hello.java
+++ src/java_personal/tt/Hello.java
@@ -8,3 +8,19 @@ public class Hello {
 	}
 
 }
+
+public class Main {
+    public static void main(String[] args) {
+        /* 기본 데이터 타입 */
+        int i = 1;
+        float f = 1.1f;
+        double d = 1.2;
+        boolean b = true;
+        char c = 'a';
+        System. out.println(i );
+        System. out.println(f );
+        System. out.println( d);
+        System. out.println(b );
+        System. out.println(c );
+    }
+}
